675f00f5c8eb984f47a0cb9bf2c705c7f607c95e
Lpfan030303 <rodri_lahut@yahoo.com.mx>
2016-04-17 21:37:02 -0500

    Cambios al script PartesCuerpo

--- Assets/Resources/Scripts/PartesCuerpo.cs
+++ Assets/Resources/Scripts/PartesCuerpo.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PartesCuerpo : MonoBehaviour
 {
@@ -7,8 +8,8 @@ public class PartesCuerpo : MonoBehaviour
     public GameObject objAnterior;
     public Vector3 posicionAnterior;
     public Vector3 rotacionAnterior;
-    public Vector3 op;
     public DistanceJoint2D distancia;
+    public float movimiento = .5f;
     // Use this for initialization
     void Start()
     {
@@ -18,35 +19,90 @@ public class PartesCuerpo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ActualizaTransformacion();
+        float verticalValue = Input.GetAxisRaw("Vertical");
+        float horizontalValue = Input.GetAxisRaw("Horizontal");
+        GeneraRotacion(Vector3.zero);
+        this.transform.position = ActualizaTransformacion();
+        if (verticalValue!=0)
+        {
+            StartCoroutine("AlcanzarSiguiente");
+            GeneraRotacion(Vector3.zero);
+            this.transform.position = ActualizaTransformacion();
+        }
+        if (horizontalValue != 0)
+        {
+            StartCoroutine("AlcanzarSiguiente");
+            GeneraRotacion(Vector3.zero);
+            this.transform.position = ActualizaTransformacion();
+        }
     }
-    void ActualizaTransformacion()
+
+    Vector3 ActualizaTransformacion()
     {
         float verticalValue = Input.GetAxisRaw("Vertical");
         float horizontalValue = Input.GetAxisRaw("Horizontal");
         Vector3 vectorTeclado = Vector3.zero;
         Vector3 posicionCuerpo;
-        op = objAnterior.GetComponent<Cabeza>().posicionAnterior;
-        if (verticalValue < 0 || verticalValue > 0)
+        posicionCuerpo = (objAnterior.GetComponent<Cabeza>().posicionAnterior);
+        if (verticalValue < 0 || verticalValue > 0) // verticalValue != 0
         {
-            posicionCuerpo = (objAnterior.GetComponent<Cabeza>().posicionAnterior) - new Vector3(0, 1, 0);
-            this.transform.position = posicionCuerpo;
+            this.transform.position = posicionCuerpo - new Vector3(0, verticalValue, 0);
+            this.transform.position += this.transform.up * (Time.deltaTime) * verticalValue;
             this.transform.eulerAngles = objAnterior.GetComponent<Cabeza>().rotacionAnterior;
+            print(objAnterior.GetComponent<Cabeza>().posicionAnterior);
+            print(this.transform.position);
         }
         else
         {
             if (horizontalValue < 0 || horizontalValue > 0)
             {
-                posicionCuerpo = (objAnterior.GetComponent<Cabeza>().posicionAnterior) - new Vector3(0, 0, 1);
-                this.transform.position = posicionCuerpo;
+                this.transform.position = posicionCuerpo - new Vector3(horizontalValue, 0, 0);
+
                 this.transform.eulerAngles = objAnterior.GetComponent<Cabeza>().rotacionAnterior;
-            }
-            else
-            {
-                this.transform.position = (objAnterior.GetComponent<Cabeza>().posicionAnterior)-new Vector3(0, 1, 0);
+                print(objAnterior.GetComponent<Cabeza>().posicionAnterior);
+                print(this.transform.position);
             }
         }
-        
- 
+        return this.transform.position;
     }
+    void GeneraRotacion(Vector3 vectorAux)
+    {
+        if (vectorAux.Equals(Vector3.zero))
+            return;
+        float dot = Vector3.Dot(this.transform.up, vectorAux);
+        if (dot < 0.05 && dot > -0.05)
+        {
+            Vector3 cross = Vector3.Cross(this.transform.up, vectorAux);
+            float angulo = Vector3.Angle(this.transform.up, vectorAux);
+            print(string.Format("{0} {1} {2}", dot, cross, angulo));
+            this.transform.eulerAngles += new Vector3(0f, 0f, angulo * (cross.z < 0 ? -1 : 1));
+        }
+    }
+    IEnumerator AlcanzarSiguiente()
+    {
+        float verticalValue = Input.GetAxisRaw("Vertical");
+        float horizontalValue = Input.GetAxisRaw("Horizontal");
+        while (true)
+        {
+            if (this.transform.position == objAnterior.GetComponent<Cabeza>().posicionAnterior)
+            {
+                while (this.transform.position != objAnterior.GetComponent<Cabeza>().posicionAnterior)
+                {
+                    if (verticalValue != 0)
+                    {
+                        this.transform.up = objAnterior.GetComponent<Cabeza>().transform.up;
+                    }
+                    if (horizontalValue != 0)
+                    {
+                        this.transform.up = objAnterior.GetComponent<Cabeza>().transform.up;
+                    }
+                }
+                break;
+            }
+            transform.position += new Vector3(0,1,0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    //if(comperatag("s")) && comparetag("wall")
+
 }
