ad5cd62facc410279dd8ddb3954005dd1546dafa
Lpfan030303 <rodri_lahut@yahoo.com.mx>
2016-04-29 22:16:46 -0500

    Cambios en PartesCuerpo y creacion del script ObtenePosicion Se creo el script de ObtenerPosicion para que no se siga siempre a la cabeza, y se termino casi por completo el script Partes del cuerpo

--- Assets/Resources/Scripts/PartesCuerpo.cs
+++ Assets/Resources/Scripts/PartesCuerpo.cs
@@ -14,6 +14,7 @@ public class PartesCuerpo : MonoBehaviour
     private float contadorTiempo;
     private float verticalValue;
     private float horizontalValue;
+    private bool inicio;
     
     // Use this for initialization
     void Start()
@@ -23,24 +24,20 @@ public class PartesCuerpo : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        verticalValue = Input.GetAxisRaw("Vertical"); // INPUT: Aquí recibes el Input y éste tendrá el valor de 1 mientras la tecla esté presionada
-        horizontalValue = Input.GetAxisRaw("Horizontal"); // INPUT: recuerda que esto sucede durante un frame del juego.
-        //Es más, aquí no nos interesa el valor del Axis, sólo queremos saber si se presionó la tecla y ya hay funciones del Input que devuelven un booleano con esta información
+    { 
         contadorTiempo += Time.deltaTime;
         if (contadorTiempo >= limiteTiempoMovimiento)
         {
             GenerarMovimiento();
-            // Recuerda los operadores booleanos, los siguientes if's quedan mejor con un OR. Está el detalle que ambos sean ciertos, esto último mejor no lo consideres.
-            // Como consejo, si dos o más if's tienen exactamente las mismas sentencias entre las llaves {} lo más probable es que necesites un OR.
-            if (verticalValue != 0)
+            if (verticalValue != 0 || horizontalValue != 0 )
             {
-                StartCoroutine("AlcanzarSiguiente");
+                inicio = true;
+                if (inicio == true)
+                {
+                    StartCoroutine("AlcanzarSiguiente");
+                }
             }
-            if (horizontalValue != 0)
-            {
-                StartCoroutine("AlcanzarSiguiente");
-            }
+            
             contadorTiempo = 0f;
         }
         
@@ -65,31 +62,18 @@ public class PartesCuerpo : MonoBehaviour
             this.transform.eulerAngles += new Vector3(0f, 0f, angulo * (cross.z < 0 ? -1 : 1));
         }
     }
-    Vector3 ActualizaTransformacion()
+    void ActualizaTransformacion()
     {
         Vector3 posicionCuerpo;
-        posicionCuerpo = (objAnterior.GetComponent<Cabeza>().posicionAnterior); 
-        if (verticalValue != 0) // INPUT: Recuerda que para este punto esta condición ya se cumplió ya que para crear la corrutina ya se preguntó si el Axis vale 1,
-        { //INPUT: cuando preguntas aquí lo más probable es que el jugador ya no tenga presionada la tecla. Lo mismo aplica para el siguiente if.
-            this.transform.eulerAngles = objAnterior.GetComponent<Cabeza>().rotacionAnterior;
-            print(objAnterior.GetComponent<Cabeza>().posicionAnterior);
-            print(this.transform.position);
-        }
-        else
-        {
-            if (horizontalValue < 0 || horizontalValue > 0) // Recuerda que es mejor usar la diferencia !=
-            {
-                this.transform.eulerAngles = objAnterior.GetComponent<Cabeza>().rotacionAnterior;
-                print(objAnterior.GetComponent<Cabeza>().posicionAnterior);
-                print(this.transform.position);
-            }
-        }
-        return this.transform.position; // No tiene caso retornar esto por que ya se puede accesar a la posición en cualquier momento
+        posicionCuerpo = (objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior); 
+        this.transform.eulerAngles = objAnterior.GetComponent<ObtenerPosicion>().rotacionAnterior;
+        print(objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior);
+        print(this.transform.position);
     }
 
     IEnumerator AlcanzarSiguiente()
     {
-            while (this.transform.position != objAnterior.GetComponent<Cabeza>().posicionAnterior) 
+            while (this.transform.position != objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior) 
             {
                 yield return new WaitForEndOfFrame();
                 ActualizaTransformacion();
