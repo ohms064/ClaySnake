619b7a8b39c08ad9681bcb281bdc6051687771f5
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2016-05-02 00:37:09 -0500

    Completo el movimiento. Se terminó los scripts de movimiento, se tiene una clase abstracta ACuerpo que servirá para comunicar todas las partes del cuerpo. Se tiene tiene como hijos, Cabeza, Cola y PartesCuerpo.

--- Assets/Resources/Scripts/PartesCuerpo.cs
+++ Assets/Resources/Scripts/PartesCuerpo.cs
@@ -2,84 +2,39 @@
 using System.Collections;
 using System;
 
-public class PartesCuerpo : MonoBehaviour
+public class PartesCuerpo : ACuerpo
 {
-    public GameObject objSiguiente;
-    public GameObject objAnterior;
-    public Vector3 posicionAnterior;
-    public Vector3 rotacionAnterior;
+    //Se heredan las variables posicionAnterior, rotacionAnterior y siguienteParte.
     public float movimiento = 10.0f;            //Velocidad de la cabeza al ser multiplicado por deltatime
     public float limiteTiempoMovimiento = 1f;
-    //
-    private float contadorTiempo;
-    private float verticalValue;
-    private float horizontalValue;
-    private bool inicio;
-    
+
     // Use this for initialization
-    void Start()
-    {
-        contadorTiempo = 0f;
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    { 
-        contadorTiempo += Time.deltaTime;
-        if (contadorTiempo >= limiteTiempoMovimiento)
-        {
-            GenerarMovimiento();
-            if (verticalValue != 0 || horizontalValue != 0 )
-            {
-                inicio = true;
-                if (inicio == true)
-                {
-                    StartCoroutine("AlcanzarSiguiente");
-                }
-            }
-            
-            contadorTiempo = 0f;
-        }
-        
-    }
-    void GenerarMovimiento()
-    {
-            posicionAnterior = this.transform.position;
-            rotacionAnterior = this.transform.eulerAngles;
-            this.transform.position += this.transform.up * (Time.deltaTime) * this.movimiento;
-       
-    }
-    void GeneraRotacion(Vector3 vectorAux)
-    {
-        if (vectorAux.Equals(Vector3.zero))
-            return;
-        float dot = Vector3.Dot(this.transform.up, vectorAux);
-        if (dot < 0.05 && dot > -0.05)
-        {
-            Vector3 cross = Vector3.Cross(this.transform.up, vectorAux);
-            float angulo = Vector3.Angle(this.transform.up, vectorAux);
-            print(string.Format("{0} {1} {2}", dot, cross, angulo));
-            this.transform.eulerAngles += new Vector3(0f, 0f, angulo * (cross.z < 0 ? -1 : 1));
-        }
-    }
-    void ActualizaTransformacion()
-    {
-        Vector3 posicionCuerpo;
-        posicionCuerpo = (objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior); 
-        this.transform.eulerAngles = objAnterior.GetComponent<ObtenerPosicion>().rotacionAnterior;
-        print(objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior);
-        print(this.transform.position);
+
+    override public void GenerarMovimiento() {
+        this.transform.position += this.transform.up * this.movimiento;  
+        siguienteParte.GenerarMovimiento();  
     }
 
-    IEnumerator AlcanzarSiguiente()
-    {
-            while (this.transform.position != objAnterior.GetComponent<ObtenerPosicion>().posicionAnterior) 
-            {
-                yield return new WaitForEndOfFrame();
-                ActualizaTransformacion();
-            }
+    IEnumerator AlcanzarSiguiente(Transform transAlcanzar) {
+        Vector3 posicionAlcanzar = transAlcanzar.position;
+        Vector3 rotacionAlcanzar = transAlcanzar.localEulerAngles;
+        while (this.transform.position != posicionAlcanzar ) {
+            yield return new WaitForFixedUpdate();
+        }
+        this.transform.localEulerAngles = rotacionAlcanzar;
+        siguienteParte.InicioGiro( this.transform ); //Iniciamos la corutina para la siguiente pieza
        
     }
-    //if(comperatag("s")) && comparetag("wall")
+    /// <summary>
+    /// Esta función es llamada desde una sección anterior del cuerpo.
+    /// </summary>
+    /// <param name="posicion"></param>
+    override public void InicioGiro( Transform transAnterior ) {
+        StartCoroutine( "AlcanzarSiguiente", transAnterior );
+    }
 
 }
